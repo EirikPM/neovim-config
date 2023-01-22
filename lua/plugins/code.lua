765d3ada6a25e726ce3d26fd56fc8a1a565183fb
Eirik Persson Masteholtet <eirik.masteholtet@gmail.com>
2023-01-22 18:08:27 +0100

    Added Harpoon

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -18,4 +18,28 @@ return {
     "alx741/vim-hindent",
     enabled = true,
   },
+
+  {
+    "ThePrimeagen/harpoon",
+    opts = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+
+      vim.keymap.set("n", "<leader>a", mark.add_file)
+      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+      vim.keymap.set("n", "<C-h>", function()
+        ui.nav_file(1)
+      end)
+      vim.keymap.set("n", "<C-t>", function()
+        ui.nav_file(2)
+      end)
+      vim.keymap.set("n", "<C-n>", function()
+        ui.nav_file(3)
+      end)
+      vim.keymap.set("n", "<C-s>", function()
+        ui.nav_file(4)
+      end)
+    end,
+  },
 }
