4b2df60360fbd26dec2527c30041dde07e5954d4
Eirik Persson Masteholtet <eirik.masteholtet@gmail.com>
2023-01-31 00:29:26 +0100

    Added ZenMode

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,22 @@
 return {
   { "neo-tree.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
+  {
+    "folke/zen-mode.nvim",
+    lazy = true,
+    init = function()
+      vim.keymap.set("n", "<leader>zz", function()
+        require("zen-mode").toggle()
+      end, { desc = "ZenMode" })
+    end,
+    opts = {
+      window = {
+        width = 100,
+        options = {
+          number = true,
+          relativenumber = true,
+        },
+      },
+    },
+  },
 }
