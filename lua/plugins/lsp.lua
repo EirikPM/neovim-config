b0dbb8f62dd77282e950de2c1370298991fea612
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2023-03-29 09:02:07 +0200

    Added formatting for terraform

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,11 @@ return {
         sources = {
           nls.builtins.formatting.stylua,
           nls.builtins.formatting.ktlint,
+          nls.builtins.diagnostics.tfsec,
+          nls.builtins.formatting.terraform_fmt,
+          nls.builtins.formatting.hclfmt,
+          nls.builtins.formatting.yamlfmt,
+          --          nls.builtins.formatting.packer,
         },
       }
     end,
