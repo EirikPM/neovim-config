4093cd4ce55963fe851c0be195221358914409b4
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-07-07 15:38:28 +0200

    Todo keybindings

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,5 +1,11 @@
 return {
 	"folke/todo-comments.nvim",
+	lazy = false,
 	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>tq", "<cmd>TodoQuickFix<cr>", desc = "Move Todo to Quickfix list" },
+		{ "<leader>tl", "<cmd>TodoLocList<cr>", desc = "Move Todo to Location list" },
+		{ "<leader>tf", "<cmd>TodoTelescope<cr>", desc = "Search project Todos" },
+	},
 	opts = {},
 }
