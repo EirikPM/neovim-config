df761748d5d35581c76cde75a9cb5bbb2f892ac3
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-07-07 21:42:31 +0200

    Add features for Rust crates

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,23 +1,51 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^4", -- Recommended
-	lazy = false, -- This plugin is already lazy
-	config = function()
-		local mason_registry = require("mason-registry")
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		lazy = false, -- This plugin is already lazy
+		config = function()
+			local mason_registry = require("mason-registry")
 
-		local codelldb = mason_registry.get_package("codelldb")
-		local extension_path = codelldb:get_install_path() .. "/extension/"
-		local codelldb_path = extension_path .. "adapter/codelldb"
-		local this_os = vim.uv.os_uname().sysname
-		-- The liblldb extension is .so for Linux and .dylib for MacOS
-		-- TODO: Make this dynamic
-		local liblldb_path = extension_path .. "lldb/lib/liblldb.so" --[[ .. (this_os == "Linux" and ".so" or ".dylib") ]]
+			local codelldb = mason_registry.get_package("codelldb")
+			local extension_path = codelldb:get_install_path() .. "/extension/"
+			local codelldb_path = extension_path .. "adapter/codelldb"
+			local this_os = vim.uv.os_uname().sysname
+			-- The liblldb extension is .so for Linux and .dylib for MacOS
+			-- TODO: Make this dynamic
+			local liblldb_path = extension_path .. "lldb/lib/liblldb.so" --[[ .. (this_os == "Linux" and ".so" or ".dylib") ]]
 
-		local cfg = require("rustaceanvim.config")
-		vim.g.rustaceanvim = {
-			dap = {
-				adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-			},
-		}
-	end,
+			local cfg = require("rustaceanvim.config")
+			vim.g.rustaceanvim = {
+				dap = {
+					adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+				},
+			}
+		end,
+	},
+	{
+		{
+			"saecki/crates.nvim",
+			tag = "stable",
+			event = { "BufRead Cargo.toml" },
+			config = function()
+				local crates = require("crates")
+				crates.setup({
+					lsp = {
+						enabled = true,
+						actions = true,
+						completion = true,
+						hover = true,
+					},
+					completion = {
+						cmp = {
+							enabled = true,
+						},
+					},
+				})
+
+				-- Keymaps
+				vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { desc = "Show Crate Features" })
+			end,
+		},
+	},
 }
