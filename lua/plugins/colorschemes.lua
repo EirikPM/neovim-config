3048bec4469649e168a1866c126a8e74f64d6f50
Eirik Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-07-07 23:26:43 +0200

    Add more colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -5,4 +5,17 @@ return {
 		lazy = false,
 		priority = 1000,
 	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{
+		"sho-87/kanagawa-paper.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 }
