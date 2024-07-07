3bdc40818ff6c5bc9bd07899dc3dcddcf26d8154
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-07-07 21:42:51 +0200

    Add crates and path to completions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -34,7 +34,8 @@ return {
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- For luasnip users.
-			}, {
+				{ name = "crates" },
+				{ name = "path" },
 				{ name = "buffer" },
 			}),
 		})
