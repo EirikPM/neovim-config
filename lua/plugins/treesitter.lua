cc21e715257460b9021626238dd4bf2517c5f253
Eirik Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-07-19 14:19:42 +0200

    Legg til treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,20 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })()
+		end,
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+			configs.setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {},
+	},
 }
