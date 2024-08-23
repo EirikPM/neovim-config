5f1364bfd161cc25878cd19dac3cef8c986d9e95
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-08-24 01:42:43 +0200

    feat(keymaps): add keymap to toggle Twilight plugin

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -2,3 +2,4 @@ local set = vim.keymap.set
 
 set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy" })
 set("n", "<leader>m", "<cmd>Mason<cr>", { desc = "Open Mason" })
+set("n", "<leader>tt", "<cmd>Twilight<cr>", { desc = "Toggle Twilight" })
