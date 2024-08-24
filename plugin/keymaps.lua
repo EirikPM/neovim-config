c2444374ee033983100afcedd7283fb280ab2a12
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-08-24 14:59:11 +0200

    feat(keymaps): add keymap for generating commit message
    Added a new keymap to generate commit messages for staged files using
    CopilotChatCommitStaged command. This enhances the workflow by
    providing a quick way to generate commit messages.

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -3,3 +3,4 @@ local set = vim.keymap.set
 set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy" })
 set("n", "<leader>m", "<cmd>Mason<cr>", { desc = "Open Mason" })
 set("n", "<leader>tt", "<cmd>Twilight<cr>", { desc = "Toggle Twilight" })
+set("n", "<leader>cm", "<cmd>CopilotChatCommitStaged<cr>", { desc = "Generate commit message staged files" })
