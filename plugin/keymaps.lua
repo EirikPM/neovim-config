0ebe55be96e534f3d4dab7fcdc3cb2e1e4ae6a0f
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2024-08-26 00:46:24 +0200

    feat(keymaps): add centered scrolling for <C-u> and <C-d>

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -2,3 +2,5 @@ local set = vim.keymap.set
 
 set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy" })
 set("n", "<leader>m", "<cmd>Mason<cr>", { desc = "Open Mason" })
+set("n", "<C-u>", "<C-u>zz", { desc = "scroll up and center" })
+set("n", "<C-d>", "<C-d>zz", { desc = "scroll down and center" })
