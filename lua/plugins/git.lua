360ccfa80cb245bd9299ff23018ad42e8505ae9e
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-03-13 22:54:07 +0100

    Add diffview

---
+++ lua/plugins/git.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with "keys" is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	{
+		"sindrets/diffview.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader><leader>v", function()
+				if next(require("diffview.lib").views) == nil then
+					vim.cmd("DiffviewOpen")
+				else
+					vim.cmd("DiffviewClose")
+				end
+			end)
+		end,
+	},
+}
