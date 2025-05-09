a6afba1f146884f4992387628d72d51598d66d07
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-05-09 21:04:18 +0200

    Remove rustowl

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -31,20 +31,6 @@ return {
 		end,
 	},
 	{
-		"cordx56/rustowl",
-		build = "cd rustowl && cargo install --path . --locked",
-		lazy = false, -- This plugin is already lazy
-		opts = {
-			client = {
-				on_attach = function(_, buffer)
-					vim.keymap.set("n", "<leader>o", function()
-						require("rustowl").toggle(buffer)
-					end, { buffer = buffer, desc = "Toggle RustOwl" })
-				end,
-			},
-		},
-	},
-	{
 		{
 			"saecki/crates.nvim",
 			tag = "stable",
