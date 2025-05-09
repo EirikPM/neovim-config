80b98fe8ca0644217b665d2773004d973caf095d
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-05-09 21:03:32 +0200

    Add git toggle blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,9 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	lazy = false,
+	keys = {
+		{ "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle current line blame" },
+		{ "<leader>dt", "<cmd>Gitsigns diffthis<cr>", desc = "Diff this" },
+	},
 	opts = {},
 }
