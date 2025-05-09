b5bb9493ecc7a14e14adf0d6621b793c74f0d0c5
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-05-09 23:45:59 +0200

    lualine: Show relative filepath

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,14 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "ayu_mirage",
+	opts = {
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					path = 1,
+				},
 			},
-		})
-	end,
+		},
+	},
 }
