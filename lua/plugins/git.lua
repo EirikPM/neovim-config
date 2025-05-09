f7fb006c8a518ba17a093f71702b1398dd17584a
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-05-09 21:03:13 +0200

    Update diffview commands

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,11 @@
+local function toggle_diffview(cmd)
+	if next(require("diffview.lib").views) == nil then
+		vim.cmd(cmd)
+	else
+		vim.cmd("DiffviewClose")
+	end
+end
+
 return {
 	{
 		"kdheepak/lazygit.nvim",
@@ -20,14 +28,39 @@ return {
 	},
 	{
 		"sindrets/diffview.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader><leader>v", function()
-				if next(require("diffview.lib").views) == nil then
-					vim.cmd("DiffviewOpen")
-				else
-					vim.cmd("DiffviewClose")
-				end
-			end)
-		end,
+		command = "DiffviewOpen",
+		-- cond = is_git_root,
+		keys = {
+			{
+				"<leader>gd",
+				function()
+					toggle_diffview("DiffviewOpen")
+				end,
+				desc = "Diff Index",
+			},
+			{
+				"<leader>gD",
+				function()
+					toggle_diffview("DiffviewOpen master..HEAD")
+				end,
+				desc = "Diff master",
+			},
+			{
+				"<leader>gf",
+				function()
+					toggle_diffview("DiffviewFileHistory %")
+				end,
+				desc = "Open diffs for current File",
+			},
+		},
+		-- config = function()
+		-- 	vim.keymap.set("n", "<leader><leader>v", function()
+		-- 		if next(require("diffview.lib").views) == nil then
+		-- 			vim.cmd("DiffviewOpen")
+		-- 		else
+		-- 			vim.cmd("DiffviewClose")
+		-- 		end
+		-- 	end)
+		-- end,
 	},
 }
