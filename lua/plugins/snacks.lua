1754fba5da55bf73a14bb6fd98155ca9b6177bd2
Eirik Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-06-17 23:15:15 +0200

    Erstatt telescope med picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,5 +7,22 @@ return {
 		indent = { enabled = true },
 		statuscolumn = { enabled = true },
 		scroll = { enabled = true },
+		animate = { enabled = true },
+		gitbrowse = { enabled = true },
+		picker = { enabled = true },
+	},
+	keys = {
+		-- find
+		{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+		-- search
+		{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+		{ "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+		{ "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+		{ "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+		{ "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix list" },
+		{ "<leader>fc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+		-- LSP
+		{ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+		{ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,51 +1,51 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-
-			"nvim-telescope/telescope-smart-history.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-		},
-		keys = {
-			{ "<leader>/", false },
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-			{ "<leader>ft", "<cmd>Telescope git_files<cr>", desc = "Find Git files" },
-			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find Files" },
-			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-			{ "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "Change Colorscheme" },
-			{
-				"<leader>fp",
-				function()
-					require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-				end,
-				desc = "Find Plugin File",
-			},
-			{ "<space>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find Current Buffer" },
-		},
-		opts = {
-			pickers = {
-				colorscheme = {
-					enable_preview = true,
-				},
-			},
-		},
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-						}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+	--  {
+	-- 	"nvim-telescope/telescope.nvim",
+	-- 	tag = "0.1.8",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	--
+	-- 		"nvim-telescope/telescope-smart-history.nvim",
+	-- 		"nvim-telescope/telescope-ui-select.nvim",
+	-- 	},
+	-- 	keys = {
+	-- 		{ "<leader>/", false },
+	-- 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+	-- 		{ "<leader>ft", "<cmd>Telescope git_files<cr>", desc = "Find Git files" },
+	-- 		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find Files" },
+	-- 		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+	-- 		{ "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "Change Colorscheme" },
+	-- 		{
+	-- 			"<leader>fp",
+	-- 			function()
+	-- 				require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+	-- 			end,
+	-- 			desc = "Find Plugin File",
+	-- 		},
+	-- 		{ "<space>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find Current Buffer" },
+	-- 	},
+	-- 	opts = {
+	-- 		pickers = {
+	-- 			colorscheme = {
+	-- 				enable_preview = true,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
+	-- {
+	-- 	"nvim-telescope/telescope-ui-select.nvim",
+	-- 	config = function()
+	-- 		require("telescope").setup({
+	-- 			extensions = {
+	-- 				["ui-select"] = {
+	-- 					require("telescope.themes").get_dropdown({
+	-- 						-- even more opts
+	-- 					}),
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 		require("telescope").load_extension("ui-select")
+	-- 	end,
+	-- },
 }
