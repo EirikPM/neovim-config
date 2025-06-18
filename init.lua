74c9f7bcf6556bdf878fafa211aeb7f3bea60d6b
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-06-19 00:11:24 +0200

    Support for opacity

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 require("config.lazy")
 require("colors")
+
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]])
