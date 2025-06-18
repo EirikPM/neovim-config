6c5dc5aec20905df8401d7109ef3c5e9881ca24f
Eirik Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-06-18 23:11:33 +0200

    Add more keymaps for snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,19 +10,38 @@ return {
 		animate = { enabled = true },
 		gitbrowse = { enabled = true },
 		picker = { enabled = true },
+		scratch = { enabled = true },
 	},
 	keys = {
 		-- find
 		{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+		{ "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+		{
+			"<leader>fc",
+			function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,
+			desc = "Find Config Files",
+		},
 		-- search
 		{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
 		{ "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
 		{ "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
 		{ "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
 		{ "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix list" },
-		{ "<leader>fc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+		{ "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
 		-- LSP
 		{ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
 		{ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+		{ "<leader>gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+		-- Other
+		{ "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+		{ "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+		{ "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
 	},
+
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function() Snacks.toggle.inlay_hints():map("<leader>uh") end,
+		})
+	end,
 }
