60fdfbc8719c4a98eda5996bb9bbfb2fd5631e06
Eirik Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-06-18 23:11:21 +0200

    Move diagnostics to the right of the screen

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,37 +1,30 @@
 return {
 	"folke/trouble.nvim",
-	opts = { use_diagnostic_signs = true },
+	dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
+	opts = {
+		signs = true,
+		modes = {
+			diagnostics = {
+				win = {
+					position = "right",
+					type = "split",
+					relative = "editor",
+					size = 60,
+				},
+			},
+		},
+	},
 	cmd = "Trouble",
 	keys = {
-		{
-			"<leader>xx",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Diagnostics (Trouble)",
-		},
-		{
-			"<leader>xX",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>cs",
-			"<cmd>Trouble symbols toggle focus=false<cr>",
-			desc = "Symbols (Trouble)",
-		},
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+		{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
 		{
 			"<leader>cl",
 			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
 			desc = "LSP Definitions / references / ... (Trouble)",
 		},
-		{
-			"<leader>xL",
-			"<cmd>Trouble loclist toggle<cr>",
-			desc = "Location List (Trouble)",
-		},
-		{
-			"<leader>xQ",
-			"<cmd>Trouble qflist toggle<cr>",
-			desc = "Quickfix List (Trouble)",
-		},
+		{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+		{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
 	},
 }
