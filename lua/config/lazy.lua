c434b24d7c665d7900b913822f659e8c6fedf12b
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-06-19 22:30:22 +0200

    Remove duplicate config of leader-key

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,12 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
@@ -41,5 +35,110 @@ require("lazy").setup({
 		autocmds = true,
 		keymaps = true,
 		-- options = true,
+	},
+
+	icons = {
+		misc = {
+			dots = "󰇘",
+		},
+		ft = {
+			octo = "",
+		},
+		dap = {
+			Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+			Breakpoint = " ",
+			BreakpointCondition = " ",
+			BreakpointRejected = { " ", "DiagnosticError" },
+			LogPoint = ".>",
+		},
+		diagnostics = {
+			Error = " ",
+			Warn = " ",
+			Hint = " ",
+			Info = " ",
+		},
+		git = {
+			added = " ",
+			modified = " ",
+			removed = " ",
+		},
+		kinds = {
+			Array = " ",
+			Boolean = "󰨙 ",
+			Class = " ",
+			Codeium = "󰘦 ",
+			Color = " ",
+			Control = " ",
+			Collapsed = " ",
+			Constant = "󰏿 ",
+			Constructor = " ",
+			Copilot = " ",
+			Enum = " ",
+			EnumMember = " ",
+			Event = " ",
+			Field = " ",
+			File = " ",
+			Folder = " ",
+			Function = "󰊕 ",
+			Interface = " ",
+			Key = " ",
+			Keyword = " ",
+			Method = "󰊕 ",
+			Module = " ",
+			Namespace = "󰦮 ",
+			Null = " ",
+			Number = "󰎠 ",
+			Object = " ",
+			Operator = " ",
+			Package = " ",
+			Property = " ",
+			Reference = " ",
+			Snippet = "󱄽 ",
+			String = " ",
+			Struct = "󰆼 ",
+			Supermaven = " ",
+			TabNine = "󰏚 ",
+			Text = " ",
+			TypeParameter = " ",
+			Unit = " ",
+			Value = " ",
+			Variable = "󰀫 ",
+		},
+	},
+	---@type table<string, string[]|boolean>?
+	kind_filter = {
+		default = {
+			"Class",
+			"Constructor",
+			"Enum",
+			"Field",
+			"Function",
+			"Interface",
+			"Method",
+			"Module",
+			"Namespace",
+			"Package",
+			"Property",
+			"Struct",
+			"Trait",
+		},
+		markdown = false,
+		help = false,
+		-- you can specify a different filter for each filetype
+		lua = {
+			"Class",
+			"Constructor",
+			"Enum",
+			"Field",
+			"Function",
+			"Interface",
+			"Method",
+			"Module",
+			"Namespace",
+			-- "Package", -- remove package since luals uses it for control flow structures
+			"Property",
+			"Struct",
+			"Trait",
+		},
 	},
 })
