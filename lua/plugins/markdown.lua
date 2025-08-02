c16c30da98e132aaab9ec483befc067a54fe39a3
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-08-02 13:27:15 +0200

    Lazyload markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,8 +1,8 @@
 return {
 	"OXY2DEV/markview.nvim",
 	opts = {},
-	lazy = false, -- Recommended
-	-- ft = "markdown" -- If you decide to lazy-load anyway
+	lazy = true, -- false Recommended
+	ft = "markdown", -- If you decide to lazy-load anyway
 
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
