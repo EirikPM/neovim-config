72693351f8127b8271285a1821dd3ac39bcddc22
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-08-02 13:26:51 +0200

    Disable treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,7 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			require("nvim-treesitter.install").update({ with_sync = true })()
-		end,
+		build = function() require("nvim-treesitter.install").update({ with_sync = true })() end,
 		config = function()
 			local configs = require("nvim-treesitter.configs")
 
@@ -16,5 +14,6 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		opts = {},
+		enabled = false,
 	},
 }
