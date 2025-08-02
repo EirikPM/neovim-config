53d09c881e296f70812fb0d3c39df8b8e15468cd
Eirik Persson Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-08-02 13:25:37 +0200

    Change trouble layout

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,17 +1,14 @@
 return {
 	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "folke/snacks.nvim" },
 	opts = {
-		use_diagnostic_signs = true,
 		modes = {
-			diagnostics = {
-				win = {
-					position = "right",
-					type = "split",
-					relative = "editor",
-					size = 60,
-				},
+			lsp = {
+				win = { position = "bottom" },
 			},
+			-- diagnostics = {
+			-- 	win = { position = "right" },
+			-- },
 		},
 	},
 	cmd = "Trouble",
@@ -27,4 +24,32 @@ return {
 		{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
 		{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
 	},
+
+	-- "folke/trouble.nvim",
+	-- dependencies = { "nvim-tree/nvim-web-devicons", "folke/snacks.nvim" },
+	-- opts = {
+	-- 	modes = {
+	-- 		diagnostics = {
+	-- 			win = {
+	-- 				position = "right",
+	-- 				type = "split",
+	-- 				relative = "editor",
+	-- 				size = 60,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
+	-- cmd = "Trouble",
+	-- keys = {
+	-- 	{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+	-- 	{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+	-- 	{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+	-- 	{
+	-- 		"<leader>cl",
+	-- 		"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+	-- 		desc = "LSP Definitions / references / ... (Trouble)",
+	-- 	},
+	-- 	{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+	-- 	{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+	-- },
 }
