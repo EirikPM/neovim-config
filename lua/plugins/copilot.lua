ddba1646f067ee1ca76cd260543be25195352aff
Eirik Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-08-15 12:38:31 +0200

    Update CopilotChat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,21 +1,18 @@
 return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
+		branch = "main",
 		dependencies = {
-			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+			{ "nvim-lua/plenary.nvim", branch = "master" },
 		},
-		build = "make tiktoken", -- Only on MacOS or Linux
+		build = "make tiktoken",
 		opts = {
-			debug = true,
+			model = "gpt-5",
 		},
 		keys = {
-			{ "<leader>cm", "<cmd>CopilotChatCommitStaged<cr>", desc = "Generate commit message staged files" },
-			{ "<leader>cc", "<cmd>CopilotChatOpen<cr>", desc = "Open Copilot chat" },
-			{ "<leader>ce", "<cmd>CopilotChatExplain<cr>", desc = "Copilot Explain current selection" },
-			{ "<leader>cd", "<cmd>CopilotChatDocs<cr>", desc = "Copilot document current selection" },
-			{ "<leader>co", "<cmd>CopilotChatOptimize<cr>", desc = "Copilot Optimzie selected code" },
+			{ "<leader>ct", "<cmd>CopilotChatToggle<cr>", desc = "Toggle CopilotChat" },
+			{ "<leader>cp", "<cmd>CopilotChatPrompts<cr>", desc = "View/Select CopilotChat prompts" },
+			{ "<leader>cm", "<cmd>CopilotChatModels<cr>", desc = "View/Select CopilotChat models" },
 		},
 	},
 }
