0c733dde647b6022c9e84e0e36172b30f49b6089
Eirik Masteholtet <eirik.masteholtet@mattilsynet.no>
2025-08-15 13:33:32 +0200

    Legg til custom prompt for renaming av variabler og funksjonsnavn

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,14 +5,30 @@ return {
 		dependencies = {
 			{ "nvim-lua/plenary.nvim", branch = "master" },
 		},
-		build = "make tiktoken",
+		build = "make tiktoken", -- Kun for Linux og Mac
 		opts = {
 			model = "gpt-5",
+
+			prompts = {
+				Rename = {
+					prompt = "Du får en markert kodeblokk som input. Oppgaven din er KUN å rename variabler, funksjoner og felt til norske, beskrivende navn.",
+					selection = function(source)
+						local select = require("CopilotChat.select")
+						return select.visual(source)
+					end,
+				},
+			},
 		},
 		keys = {
-			{ "<leader>ct", "<cmd>CopilotChatToggle<cr>", desc = "Toggle CopilotChat" },
-			{ "<leader>cp", "<cmd>CopilotChatPrompts<cr>", desc = "View/Select CopilotChat prompts" },
-			{ "<leader>cm", "<cmd>CopilotChatModels<cr>", desc = "View/Select CopilotChat models" },
+			{ "<leader>ct", ":CopilotChatToggle<CR>", desc = "Toggle CopilotChat" },
+
+			{ "<leader>cr", ":CopilotChatRename<CR>", mode = "v", desc = "CopilotChat - Rename" },
+			{ "<leader>cR", ":CopilotChatReview<CR>", mode = "v", desc = "CopilotChat - Review code" },
+			{ "<leader>ct", ":CopilotChatTestsCR>", mode = "v", desc = "CopilotChat - Write tests" },
+			{ "<leader>cd", ":CopilotChatToggle<CR>", mode = "v", desc = "CopilotChat - Write documentation" },
+
+			{ "<leader>cp", ":CopilotChatPrompts<CR>", desc = "View/Select CopilotChat Prompts" },
+			{ "<leader>cm", ":CopilotChatModels<CR>", desc = "View/Select CopilotChat Models" },
 		},
 	},
 }
